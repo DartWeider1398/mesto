9baf6f72625b05b5b9d96595ee5dc5e0019da6d6
DartWeider1398 <107215329+DartWeider1398@users.noreply.github.com>
2022-09-25 17:36:28 +0300

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,27 @@
 function like(elem) {
-    elem.classList.toggle("elements__like-button_active");
+  elem.classList.toggle("elements__like-button_active");
 }
 
 // Получил элементы
 const popupOpenButton = document.querySelector(".profile__avatar-image-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
+const popupSaveButton = document.querySelector(".popup__save-button");
+const popupName = document.querySelector(".popup__name");
+const popupJob = document.querySelector(".popup__description");
 
 const popupToggle = function (event) {
   popup.classList.toggle("popup__active");
-  console.log("нажали на кнопку");
+  popupName.value = profileName.innerHTML;
+  popupJob.value = profileJob.innerHTML;
+};
+
+const popupChangeInfo = function (event) {
+  profileName.innerHTML = popupName.value;
+  profileJob.innerHTML = popupJob.value;
+  popup.classList.toggle("popup__active");
 };
 
 // Навесить слушатель на клик по кнопке 'О проекте'
@@ -18,22 +30,5 @@ popupOpenButton.addEventListener("click", popupToggle);
 // Навесил слушатель на клик по крестику
 popupCloseButton.addEventListener("click", popupToggle);
 
-// // Бонусики
-// const header = document.querySelector(".header");
-// header.addEventListener("click", function (event) {
-//   console.log("нажали на хедер");
-//   console.log(event.target);
-//   console.log(event.currentTarget);
-// });
+popupSaveButton.addEventListener("click", popupChangeInfo);
 
-// // Применение всплытия
-// const closePopupByClickingOnOverlay = function (event) {
-//   console.log(event.target);
-//   console.log(event.currentTarget);
-
-//   if (event.target == event.currentTarget) {
-//     popupToggle();
-//   }
-// };
-
-// popup.addEventListener("click", closePopupByClickingOnOverlay);
